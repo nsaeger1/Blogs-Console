d2633621afd2986967bc71443e4da013ec908020
lawl281 <saegernick@gmail.com>
2020-05-31 14:19:02 -0500

    assign 11

--- Models/BloggingContext.cs
+++ Models/BloggingContext.cs
@@ -4,7 +4,9 @@ namespace BlogsConsole.Models
 {
     public class BloggingContext : DbContext
     {
-        public BloggingContext() : base("name=BlogContext") { }
+        public BloggingContext() : base("name=BlogContext")
+        {
+        }
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
@@ -14,10 +16,43 @@ namespace BlogsConsole.Models
             this.Blogs.Add(blog);
             this.SaveChanges();
         }
-        
+
+        public void EditBlog(Blog blog, string title)
+        {
+            blog.Name = title;
+            this.SaveChanges();
+        }
+
+        public void RemoveBlog(Blog blog)
+        {
+            this.Blogs.Remove(blog);
+            this.SaveChanges();
+        }
+
         public void AddPost(Post post)
         {
             this.Posts.Add(post);
+            this.SaveChanges();
+        }
+
+        public void EditPost(Post post, string title, string content)
+        {
+            if (!title.Equals(""))
+            {
+                post.Title = title;
+            }
+
+            if (!content.Equals(""))
+            {
+                post.Content = content;
+            }
+
+            this.SaveChanges();
+        }
+
+        public void RemovePost(Post post)
+        {
+            this.Posts.Remove(post);
             this.SaveChanges();
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using BlogsConsole.Models;
 using System;
 using System.Collections;
 using System.ComponentModel.Design;
+using System.Data.Entity;
 using System.Linq;
 
 namespace BlogsConsole
@@ -26,19 +27,21 @@ namespace BlogsConsole
                     Console.WriteLine("2) Add Blog");
                     Console.WriteLine("3) Create Post");
                     Console.WriteLine("4) Display Post");
+                    Console.WriteLine("5) Edit Blog");
+                    Console.WriteLine("6) Delete Blog");
+                    Console.WriteLine("7) Edit Post");
+                    Console.WriteLine("8) Delete Post");
                     Console.WriteLine("9) Exit");
                     Console.Write("Enter your selection: ");
                     choice = PromptForChoice();
+
 
                     switch (choice)
                     {
                         case 1:
                             // Display all Blogs from the database
                             Console.WriteLine("All blogs in the database:");
-                            foreach (var item in query)
-                            {
-                                Console.WriteLine($"{item.BlogId}) {item.Name}");
-                            }
+                            BlogsQuery();
 
                             break;
                         case 2:
@@ -56,10 +59,7 @@ namespace BlogsConsole
                         case 3:
                             int requestedBlog;
                             string postTitle = "", postContent = "";
-                            foreach (var item in query)
-                            {
-                                Console.WriteLine($"{item.BlogId}) {item.Name}");
-                            }
+                            BlogsQuery();
 
                             Console.Write("Select a blog: ");
                             requestedBlog = PromptForChoice();
@@ -84,37 +84,60 @@ namespace BlogsConsole
                             break;
                         case 4:
                             Console.WriteLine("0) Posts from all blogs");
-                            foreach (var item in query)
-                            {
-                                Console.WriteLine($"{item.BlogId}) Posts from {item.Name}");
-                            }
-
+                            BlogsQuery();
                             Console.Write("Select a blog: ");
                             requestedBlog = PromptForChoice();
-                            if (requestedBlog == 0)
-                            {
-                                foreach (var item in posts)
-                                {
-                                    Console.WriteLine($"Blog: {item.Blog.Name}");
-                                    Console.WriteLine($"Title: {item.Title}");
-                                    Console.WriteLine($"Content: {item.Content}");
-                                }
-                            }
-                            else
-                            {
-                                var selectedBlog = posts.Where(b => b.BlogId == requestedBlog);
-
-                                
-                                    foreach (var thing in selectedBlog)
-                                    {
-                                        Console.WriteLine($"Blog: {thing.Blog.Name}");
-                                        Console.WriteLine($"Title: {thing.Title}");
-                                        Console.WriteLine($"Content: {thing.Content}");  
-                                    }
-                                
-                            }
+                            PostsQuery(requestedBlog);
 
                             break;
+                        case 5:
+                            BlogsQuery();
+                            Console.Write("Select a blog: ");
+                            requestedBlog = PromptForChoice();
+                            var editingBlog = query.FirstOrDefault(b => b.BlogId == requestedBlog);
+                            string newBlogName;
+                            do
+                            {
+                                Console.Write("Enter a new name for  {0}: ", editingBlog.Name);
+                                newBlogName = Console.ReadLine();
+                            } while (newBlogName.Length <= 0);
+
+                            db.EditBlog(editingBlog, newBlogName);
+                            break;
+                        case 6:
+                            BlogsQuery();
+                            Console.Write("Select a blog: ");
+                            requestedBlog = PromptForChoice();
+                            var removingBlog = query.FirstOrDefault(b => b.BlogId == requestedBlog);
+                            db.RemoveBlog(removingBlog);
+                            break;
+                        case 7:
+                            BlogsQuery();
+                            Console.Write("Select a blog: ");
+                            requestedBlog = PromptForChoice();
+                            PostsQuery(requestedBlog);
+                            Console.Write("Select a post: ");
+                            int requestedPost = PromptForChoice();
+                            var editingPost = posts.FirstOrDefault(p => p.PostId == requestedPost);
+                            Console.Write("Enter a new Title for {0} or leave blank to keep current: ",
+                                editingPost.Title);
+                            string newPostTitle = Console.ReadLine();
+                            Console.Write("Enter new Content for {0} or leave blank to keep current: ",
+                                editingPost.Title);
+                            string newPostContent = Console.ReadLine();
+                            db.EditPost(editingPost, newPostTitle, newPostContent);
+                            break;
+                        case 8:
+                            BlogsQuery();
+                            Console.Write("Select a blog: ");
+                            requestedBlog = PromptForChoice();
+                            PostsQuery(requestedBlog);
+                            Console.Write("Select a post: ");
+                            requestedPost = PromptForChoice();
+                            var removingPost = posts.FirstOrDefault(p => p.PostId == requestedPost);
+                            db.RemovePost(removingPost);
+                            break;
+
                         case 9:
                             Console.WriteLine("Goodbye");
                             logger.Info("Program ended");
@@ -128,6 +151,52 @@ namespace BlogsConsole
                 logger.Error(ex.Message);
             }
         }
+
+        private static void PostsQueryTest(int requestedBlog)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void BlogsQuery()
+        {
+            var blogs = new BloggingContext().Blogs;
+            foreach (var blog in blogs)
+            {
+                Console.WriteLine($"{blog.BlogId}) {blog.Name}");
+            }
+        }
+
+        private static void PostsQuery(int requestedBlog)
+        {
+            var blogs = new BloggingContext().Blogs;
+            var posts = new BloggingContext().Posts;
+
+            if (requestedBlog == 0)
+            {
+                foreach (var blog in blogs)
+                {
+                    foreach (var post in posts)
+                    {
+                        Console.WriteLine($"Blog: {blog.Name}");
+                        Console.WriteLine($"Title: {post.Title}");
+                        Console.WriteLine($"Content: {post.Content}");
+                    }
+                }
+            }
+            else
+            {
+                var selectedBlog = posts.Where(b => b.BlogId == requestedBlog);
+
+
+                foreach (var post in selectedBlog)
+                {
+                    Console.WriteLine($"Post ID: {post.PostId}");
+                    Console.WriteLine($"Title: {post.Title}");
+                    Console.WriteLine($"Content: {post.Content}");
+                }
+            }
+        }
+
 
         private static int PromptForChoice()
         {
